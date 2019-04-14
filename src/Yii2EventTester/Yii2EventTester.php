1a65426f9bb7f6b38f6c4ab09e3476ca6457eac2
cjwind <cwentsai@gmail.com>
2019-04-14 16:39:35 +0800

    Extract assertObjectEventTriggered()

--- src/Yii2EventTester/Yii2EventTester.php
+++ src/Yii2EventTester/Yii2EventTester.php
@@ -3,6 +3,10 @@ namespace cjwind\Yii2EventTester;
 
 class Yii2EventTester extends \Codeception\Module {
     public function assertEventTriggered($object, $eventName, $testedFunction) {
+        $this->assertObjectEventTriggered($object, $eventName, $testedFunction);
+    }
+
+    private function assertObjectEventTriggered($object, $eventName, $testedFunction) {
         $called = false;
         $object->on($eventName, function ($event) use (&$called) {
             $called = true;
