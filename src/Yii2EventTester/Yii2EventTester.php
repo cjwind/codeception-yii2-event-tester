1f49a6b6359702db83a59d1fc5c0fe1d550b0aab
cjwind <cwentsai@gmail.com>
2019-04-14 16:54:18 +0800

    Implement class event assertion

--- src/Yii2EventTester/Yii2EventTester.php
+++ src/Yii2EventTester/Yii2EventTester.php
@@ -2,8 +2,29 @@
 namespace cjwind\Yii2EventTester;
 
 class Yii2EventTester extends \Codeception\Module {
-    public function assertEventTriggered($object, $eventName, $testedFunction) {
-        $this->assertObjectEventTriggered($object, $eventName, $testedFunction);
+    public function assertEventTriggered($mixed, $eventName, $testedFunction) {
+        $type = gettype($mixed);
+
+        if ($type == 'string') {
+            $this->assertClassEventTriggered($mixed, $eventName, $testedFunction);
+        }
+        else if ($type == 'object') {
+            $this->assertObjectEventTriggered($mixed, $eventName, $testedFunction);
+        }
+        else {
+            throw new \Exception('$mixed should be object or class name.');
+        }
+    }
+
+    private function assertClassEventTriggered($class, $eventName, $testedFunction) {
+        $called = false;
+        \yii\base\Event::on($class, $eventName, function ($event) use (&$called) {
+            $called = true;
+        });
+
+        $testedFunction();
+
+        \PHPUnit_Framework_Assert::assertTrue($called, 'Event ' . $eventName . ' of class ' . $class . ' should be triggered.');
     }
 
     private function assertObjectEventTriggered($object, $eventName, $testedFunction) {
