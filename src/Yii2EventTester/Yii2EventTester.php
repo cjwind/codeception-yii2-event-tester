cfe0fed3e79ac3fd6f7357ad1fad983ffd48acee
cjwind <cwentsai@gmail.com>
2019-04-14 18:02:53 +0800

    Detach event handler in assert

--- src/Yii2EventTester/Yii2EventTester.php
+++ src/Yii2EventTester/Yii2EventTester.php
@@ -18,23 +18,30 @@ class Yii2EventTester extends \Codeception\Module {
 
     private function assertClassEventTriggered($class, $eventName, $testedFunction) {
         $called = false;
-        \yii\base\Event::on($class, $eventName, function ($event) use (&$called) {
+        $handler = function ($event) use (&$called) {
             $called = true;
-        });
+        };
+
+        \yii\base\Event::on($class, $eventName, $handler);
 
         $testedFunction();
 
         \PHPUnit_Framework_Assert::assertTrue($called, 'Event ' . $eventName . ' of class ' . $class . ' should be triggered.');
+
+        \yii\base\Event::off($class, $eventName, $handler);
     }
 
     private function assertObjectEventTriggered($object, $eventName, $testedFunction) {
         $called = false;
-        $object->on($eventName, function ($event) use (&$called) {
+        $handler = function ($event) use (&$called) {
             $called = true;
-        });
+        };
+        $object->on($eventName, $handler);
 
         $testedFunction();
 
         \PHPUnit_Framework_Assert::assertTrue($called, 'Event ' . $eventName . ' should be triggered.');
+
+        $object->off($eventName, $handler);
     }
 }
