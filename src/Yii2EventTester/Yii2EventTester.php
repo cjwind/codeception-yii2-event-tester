527d0bc7c89c2da44fa7d4606f24cbf3cadf8f6d
cjwind <cwentsai@gmail.com>
2019-04-27 15:50:47 +0800

    Add comment for the assertion function

--- src/Yii2EventTester/Yii2EventTester.php
+++ src/Yii2EventTester/Yii2EventTester.php
@@ -2,6 +2,13 @@
 namespace cjwind\Yii2EventTester;
 
 class Yii2EventTester extends \Codeception\Module {
+    /**
+     * Assert whether event is triggerd.
+     *
+     * @param [mixed] $mixed Object or class name.
+     * @param [string] $eventName
+     * @param [callable] $testedFunction Do something should trigger event in this function.
+     */
     public function assertEventTriggered($mixed, $eventName, $testedFunction) {
         $checker = new EventChecker;
 
